a0047b8623318f3ea8f4f3d45ebf9f63018c7cfe
Kenny Chung <kcmac@Kennys-MacBook-Pro.local>
2018-07-26 15:16:36 -1000

    fixing redirectory

--- routes/products.js
+++ routes/products.js
@@ -9,7 +9,7 @@ router.use(bodyParser.json());
 
 const indexPage = {
   showProducts: true,
-  products:productDB.all(),
+  products: productDB.all(),
 }
 
 
@@ -25,7 +25,7 @@ router.get(`/new`, (req, res) => {
 router.get(`/:id`, (req, res) => {
   let id = req.params.id;
   productDB.all().map(elem => {
-    if (elem.id === Number(id)){
+    if (elem.id === Number(id)) {
       res.render(`product`, {
         product: elem,
       })
@@ -34,7 +34,6 @@ router.get(`/:id`, (req, res) => {
 });
 
 router.get(`/:id/edit`, (req, res) => {
-  // let id = req.params.id;
   res.render('edit');
 });
 
@@ -42,17 +41,14 @@ router.get(`/:id/edit`, (req, res) => {
 //post items
 router.post(`/`, (req, res) => {
   if (!req.body.name) {
-    // res.status(404).send('Product has no name!');
-    res.redirect(303, `/products/new`)
+    res.redirect(303, `/products/new`);
 
   } else if (isNaN(parseInt(req.body.price))) {
-    // res.status(404).send('Please input a number for price');
-    res.redirect(303, `/products/new`)
+    res.redirect(303, `/products/new`);
 
   } else if (req.body.inventory < 1) {
-    // res.status(404).send('Please input an inventory value higher than 1');
-    res.redirect(303, `/products/new`)
-    
+    res.redirect(303, `/products/new`);
+
   } else {
     let newProduct = {};
     newProduct.id = generateId();
@@ -68,24 +64,34 @@ router.post(`/`, (req, res) => {
 
 //put items
 router.put(`/:id`, (req, res) => {
+  let putSuccessful = false;
   let id = req.params.id;
   productDB.all().map(elem => {
-    if (elem.id === Number(id)){
+    if (elem.id === Number(id)) {
       elem.name = req.body.name;
+      putSuccessful = true;
       res.redirect(303, `/products/${req.params.id}`)
-    } 
+    }
+    if (putSuccessful === false) {
+      res.redirect(303, `/products/${req.params.id}/edit`);
+    }
   })
 });
 //if ID doesn't exist, server hangs, need to render home but will throw header error if array length is > 1
 
 //delete items
 router.delete(`/:id`, (req, res) => {
+  let deleteSuccessful = false;
   let id = req.params.id;
   productDB.all().map(elem => {
-    if (elem.id === Number(id)){
+    if (elem.id === Number(id)) {
       productDB.remove(elem);
+      deleteSuccessful = true;
       res.render('index', indexPage);
     }
+    // if (deleteSuccessful === false){
+    //   res.render(303, `/products/${req.params.id}/edit`);
+    // }
   })
 });
 //same problem as PUT
